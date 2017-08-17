f6d04f39f4ca45eef2f201eda6f4e4be2f65a107
wilber-mndz <mendezwilber94@gmail.com>
2017-08-16 21:20:29 -0600

    Se modifico el formulario Se empezo procedimiento para encontrar la ecuacuion lineal incompleto aun

--- form_main.py
+++ form_main.py
@@ -17,11 +17,11 @@ import wx.xrc
 class form1 ( wx.Frame ):
 
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 680,400 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 680,450 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
 		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
 
-		fgSizer6 = wx.FlexGridSizer( 2, 1, 0, 0 )
+		fgSizer6 = wx.FlexGridSizer( 3, 1, 0, 0 )
 		fgSizer6.SetFlexibleDirection( wx.BOTH )
 		fgSizer6.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
 
@@ -89,19 +89,19 @@ class form1 ( wx.Frame ):
 
 		bSizer1 = wx.BoxSizer( wx.VERTICAL )
 
-		self.btnEditar = wx.Button( self, wx.ID_ANY, u"Editar", wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer1.Add( self.btnEditar, 0, wx.ALL, 5 )
-
 		self.btnEliminar = wx.Button( self, wx.ID_ANY, u"Eliminar", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.btnEliminar.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOWFRAME ) )
-
 		bSizer1.Add( self.btnEliminar, 0, wx.ALL, 5 )
+
+		self.btnNuevo = wx.Button( self, wx.ID_ANY, u"Nuevo", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.btnNuevo.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOWFRAME ) )
+
+		bSizer1.Add( self.btnNuevo, 0, wx.ALL, 5 )
 
 
 		bSizer1.AddSpacer( ( 0, 0), 1, wx.EXPAND, 5 )
 
-		self.btnNuevo = wx.Button( self, wx.ID_ANY, u"Nuevo", wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer1.Add( self.btnNuevo, 0, wx.ALL, 5 )
+		self.btnCalcular = wx.Button( self, wx.ID_ANY, u"Calcular", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer1.Add( self.btnCalcular, 0, wx.ALL, 5 )
 
 
 		fgSizer8.Add( bSizer1, 1, wx.EXPAND, 5 )
@@ -109,11 +109,41 @@ class form1 ( wx.Frame ):
 
 		fgSizer8.AddSpacer( ( 0, 0), 1, wx.EXPAND, 5 )
 
-		self.btnMostrar = wx.Button( self, wx.ID_ANY, u"Mostrar gráfica", wx.DefaultPosition, wx.DefaultSize, 0 )
-		fgSizer8.Add( self.btnMostrar, 0, wx.ALL, 5 )
-
 
 		fgSizer6.Add( fgSizer8, 1, wx.EXPAND, 5 )
+
+		fgSizer4 = wx.FlexGridSizer( 2, 6, 0, 0 )
+		fgSizer4.SetFlexibleDirection( wx.BOTH )
+		fgSizer4.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+
+
+		fgSizer4.AddSpacer( ( 40, 0), 1, wx.EXPAND, 5 )
+
+		self.label1 = wx.StaticText( self, wx.ID_ANY, u"Ecuacion lineal", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.label1.Wrap( -1 )
+		fgSizer4.Add( self.label1, 0, wx.ALL, 15 )
+
+		self.txtEcuLineal = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 160,-1 ), 0 )
+		fgSizer4.Add( self.txtEcuLineal, 0, wx.ALL, 5 )
+
+		self.m_staticText5 = wx.StaticText( self, wx.ID_ANY, u"Ecuacion no lineal", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText5.Wrap( -1 )
+		fgSizer4.Add( self.m_staticText5, 0, wx.ALL, 15 )
+
+		self.txtEcuNoLin = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 160,-1 ), 0 )
+		fgSizer4.Add( self.txtEcuNoLin, 0, wx.ALL, 5 )
+
+
+		fgSizer4.AddSpacer( ( 40, 0), 1, wx.EXPAND, 5 )
+
+
+		fgSizer4.AddSpacer( ( 40, 0), 1, wx.EXPAND, 5 )
+
+		self.btnMostrar = wx.Button( self, wx.ID_ANY, u"Mostrar gráfica", wx.DefaultPosition, wx.DefaultSize, 0 )
+		fgSizer4.Add( self.btnMostrar, 0, wx.ALL, 5 )
+
+
+		fgSizer6.Add( fgSizer4, 1, wx.EXPAND, 5 )
 
 
 		self.SetSizer( fgSizer6 )
@@ -124,9 +154,9 @@ class form1 ( wx.Frame ):
 		# Connect Events
 		self.btnAgregar.Bind( wx.EVT_BUTTON, self.Agregar )
 		self.ListCtrl.Bind( wx.EVT_LIST_ITEM_SELECTED, self.Seleccionar )
-		self.btnEditar.Bind( wx.EVT_BUTTON, self.Editar )
 		self.btnEliminar.Bind( wx.EVT_BUTTON, self.Eliminar )
 		self.btnNuevo.Bind( wx.EVT_BUTTON, self.Nuevo )
+		self.btnCalcular.Bind( wx.EVT_BUTTON, self.Calcular )
 		self.btnMostrar.Bind( wx.EVT_BUTTON, self.Mostrar )
 
 	def __del__( self ):
@@ -140,13 +170,13 @@ class form1 ( wx.Frame ):
 	def Seleccionar( self, event ):
 		event.Skip()
 
-	def Editar( self, event ):
-		event.Skip()
-
 	def Eliminar( self, event ):
 		event.Skip()
 
 	def Nuevo( self, event ):
+		event.Skip()
+
+	def Calcular( self, event ):
 		event.Skip()
 
 	def Mostrar( self, event ):
--- main.py
+++ main.py
@@ -20,12 +20,12 @@ class Principal(form_main.form1):
         # Iniciamos la clase superior (El formulario)
         form_main.form1.__init__(self, parent)
         # Declaramos variables de uso global
-        self.fila = 0
+        self.fila = 8  # Almacenamos el numero de datos iniciales
         self.filaSelec = ""
 
         # Agregamos columnas a nuestro list control.
-        self.ListCtrl.InsertColumn(1, "Desplazamientos", width=250)
-        self.ListCtrl.InsertColumn(2, "Fuerza 10⁴N", width=250)
+        self.ListCtrl.InsertColumn(1, "Desplazamientos", width=245)
+        self.ListCtrl.InsertColumn(2, "Fuerza 10⁴N", width=245)
 
         # Cargamos problema inicial
         self.Despla = [10, 20, 30, 40, 50, 60, 70, 80]
@@ -54,13 +54,13 @@ class Principal(form_main.form1):
         # Obtenemos el indice de la fila seleccionada
         self.filaSelec = self.ListCtrl.GetFocusedItem()
         self.elementoFila = self.ListCtrl.GetItemText(self.filaSelec)
-        self.fila -= 1
 
     def Eliminar(self, event):
         """Elimina de la lista el elemento seleccionado."""
         # Comprobamos que se haya seleccionado un elemento
         if self.elementoFila != "":
             self.ListCtrl.DeleteItem(self.filaSelec)
+        self.fila -= 1
 
     def Nuevo(self, event):
         """Vacia todos los campos para ingresar un nuevo problema."""
@@ -68,22 +68,42 @@ class Principal(form_main.form1):
         self.txtDespl.SetValue("")      # Limpiamos TextCtrl
         self.txtFuerza.SetValue("")
         self.txtDespl.SetFocus()        # Mandamos el foco a txtDespl
+        self.fila = 0                   # Reiniciamos el numero de filas
 
     def Mostrar(self, event):
         """Metodo que mostrara la grafica de los datos ingresados."""
-        self.Despla = []
-        self.Fuerza = []
-        # Agregamos las columnas del ListCtrl en listas
-        for i in range(0, self.ListCtrl.GetItemCount()):
-            self.Despla.append(self.ListCtrl.GetItemText(i, 0))
-            self.Fuerza.append(self.ListCtrl.GetItemText(i, 1))
-
+        self.Obtener_Datos()
         # Convertimos nuestras listas en arreglos
         ArrayDespl = np.array(self.Despla)
         ArrayFuerza = np.array(self.Fuerza)
         # Graficamos nuestros arreglos
         plt.plot(ArrayDespl, ArrayFuerza)
         plt.show()
+
+    def Calcular(self, event):
+        """Metodo para calcular la ecuacion lineal y no lineal."""
+        self.Obtener_Datos()  # Obtenemos los datos del ListCtrl
+        self.DespXFuerz = []  # Lista que almacenara la multiplacion
+
+        # Multiplicamos fuerza por desplazamiento
+        for i in range(0, len(self.Despla)):
+            self.DespXFuerz.append(round((float(self.Despla[i]) *
+                                   float(self.Fuerza[i])), 4))
+        self.suma = 0
+        # Calculamos la suma de los productos de desplazamiento * fuerza
+        for i in self.DespXFuerz:
+            self.suma += i
+
+        print self.suma
+
+    def Obtener_Datos(self):
+        """"Metodo para pasar los datos del ListCtrl a listas."""
+        self.Despla = []
+        self.Fuerza = []
+        # Agregamos las columnas del ListCtrl en listas
+        for i in range(0, self.ListCtrl.GetItemCount()):
+            self.Despla.append(self.ListCtrl.GetItemText(i, 0))
+            self.Fuerza.append(self.ListCtrl.GetItemText(i, 1))
 
 
 class MyApp(wx.App):
